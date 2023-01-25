03c3340d80db1e1ed288a763f64a33a932d460ba
Paulo0803 <113394154+Paulo0803@users.noreply.github.com>
2023-01-25 09:33:50 -0300

    commit handlebars

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
 const express = require("Express");
 const app = express();
-const handlebars = require("express-handlebars")
+const Handlebars = require('express-handlebars')
+const Sequelize = require('sequelize')
+
 
 //configurar
     //Template Engine
-    app.engine('handlebars', handlebars({defaultLayout: 'main'}))
-    app.set('View engine', 'handlebars')
+    app.engine('handlebars', Handlebars({ defaultLayout: 'main' }))
+    app.set('view engine', 'handlebars')
 
-app.get("/", function(req, res){
+    //conexão com o banco de dados mysql
+    const sequelize = new Sequelize('test', 'root', 'Paulo0108*', {
+        host: 'localhost',
+        dialect: 'mysql'
+    })
+
+//ROTAS
+app.get('/cad', (req, res) => {
+    res.send('Rota Ativa')
+})
+
+
+app.get("/", function (req, res) {
     res.sendFile(__dirname + "/html/index.html")
 })
 
-app.get("/sobre", function(req, res){
+app.get("/sobre", function (req, res) {
     res.sendFile(__dirname + "/html/sobre.html")
 })
 
-app.get("/blog", function(req, res){
+app.get("/blog", function (req, res) {
     res.send('Bem vindo ao meu blog!')
 })
 
-app.get("/ola/:nome/:cargo/:cor", (req, res) =>{
-    res.send("<h1>Ola senhor "+req.params.nome+"</h1>"+"<h2>Seu cargo atual é: "+req.params.cargo+"</h2>"+'<h3>Sua cor favorita registrada é: '+req.params.cor+"</h3>");
+app.get("/ola/:nome/:cargo/:cor", (req, res) => {
+    res.send("<h1>Ola senhor " + req.params.nome + "</h1>" + "<h2>Seu cargo atual é: " + req.params.cargo + "</h2>" + '<h3>Sua cor favorita registrada é: ' + req.params.cor + "</h3>");
 })
 
-app.listen(8081, function(){
+app.listen(8081, function () {
     console.log("servidor rodando na url http://localhost:8081/")
 });
